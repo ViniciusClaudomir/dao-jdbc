026affb0955185df65c1cabd9bf61bf26562bd4d
vinicius claudomir <viniciusclaudomir@gmail.com>
2022-06-11 12:30:24 -0300

    refactor: create instancies of Seller and Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,11 +9,10 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		new DaoFactory();
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("==== test 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 	}
